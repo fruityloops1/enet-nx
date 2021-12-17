e38f318861596918ac12421ef409c63839827f50
OatmealDome <julian@oatmealdome.me>
2021-12-17 03:35:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# enet-nx
+
+This is a partial port of ENet to NintendoSDK, tested specifically with the Nintendo Switch.
+
+Connecting to a host and communicating it works. However, any functionality that has to do with creating a server is not implemented.
+
+While this was developed for use with [Starlight](https://github.com/shadowninja108/Starlight/) and reverse engineered headers, it will likely work fine with the official SDK if you have access to it (though you might have to fix the includes and some constant names).
+
+## Copyright
+
+Copyright (c) 2002-2020 Lee Salzman
+
+Copyright (c) 2021 OatmealDome
---
+++ include/nx.h
@@ -0,0 +1,33 @@
+/** 
+ @file  nx.h
+ @brief ENet NX (NintendoSDK) header
+*/
+#ifndef __ENET_NX_H__
+#define __ENET_NX_H__
+
+#include "nn/socket.h"
+
+typedef s32 ENetSocket;
+
+#define ENET_SOCKET_NULL -1
+
+#define ENET_HOST_TO_NET_16(value) (nnsocketInetHtons(value)) /**< macro that converts host to net byte-order of a 16-bit value */
+#define ENET_HOST_TO_NET_32(value) (nnsocketInetHtonl(value)) /**< macro that converts host to net byte-order of a 32-bit value */
+
+#define ENET_NET_TO_HOST_16(value) (nnsocketInetNtohs(value)) /**< macro that converts net to host byte-order of a 16-bit value */
+#define ENET_NET_TO_HOST_32(value) (nnsocketInetNtohl(value)) /**< macro that converts net to host byte-order of a 32-bit value */
+
+typedef struct
+{
+    void * data;
+    size_t dataLength;
+} ENetBuffer;
+
+#define ENET_CALLBACK
+
+#define ENET_API extern
+
+typedef void ENetSocketSet;
+
+#endif /* __ENET_NX_H__ */
+
